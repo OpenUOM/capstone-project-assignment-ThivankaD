58a731a3287010d880c405f6a3cb60c701ad5dfa
ThivankaD <121705081+ThivankaD@users.noreply.github.com>
2023-09-30 17:58:00 +0530

    Add files via upload

---
+++ student-table.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { Router,NavigationExtras } from '@angular/router';
+import { faTrash, faPlus, faPenSquare } from '@fortawesome/free-solid-svg-icons';
+import {AppServiceService} from '../../app-service.service';
+@Component({
+  selector: 'app-student-table',
+  templateUrl: './student-table.component.html',
+  styleUrls: ['./student-table.component.css']
+})
+export class StudentTableComponent implements OnInit {
+
+  faTrash = faTrash;
+  faPlus = faPlus;
+  faPenSquare = faPenSquare;
+  studentData: any;
+  selected: any;
+
+  constructor(private service : AppServiceService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.getStudentData();
+  }
+
+  addNewStudent(){
+    this.router.navigate(['addStudent'])
+  }
+
+  editStudent(id){
+    const navigationExtras: NavigationExtras = {
+      state: {
+        id : id
+      }
+    };
+    this.router.navigate(['editStudent'], navigationExtras )
+  }
+
+  getStudentData(){
+    this.service.getStudentData().subscribe((response)=>{
+      this.studentData = Object.keys(response).map((key) => [response[key]]);
+    },(error)=>{
+      console.log('ERROR - ', error)
+    })
+  }
+
+  deleteStudent(itemid){
+    const student = {
+      id: itemid
+    }
+    this.service.deleteStudent(student).subscribe((response)=>{
+      this.getStudentData()
+    })
+  }
+
+  search(query) {
+    let filteredData = [];
+  
+    if (!query) {
+      // Retrieve all student data
+      this.getStudentData();
+    } else {
+      // Filter studentData based on query matching student names
+      filteredData = this.studentData.filter((student) =>
+        student[0].name.toLowerCase().includes(query.toLowerCase())
+      );
+  
+      this.studentData = filteredData;
+    }
+  }
+
+}
