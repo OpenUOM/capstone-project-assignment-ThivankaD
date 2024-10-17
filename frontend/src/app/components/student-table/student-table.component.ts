51a7018f787b2355d54a5f2d3fb506ffbd675faa
ThivankaD <121705081+ThivankaD@users.noreply.github.com>
2024-10-17 09:38:45 +0530

    Success

--- frontend/src/app/components/student-table/student-table.component.ts
+++ frontend/src/app/components/student-table/student-table.component.ts
@@ -33,6 +33,13 @@ export class StudentTableComponent implements OnInit {
     };
     this.router.navigate(['editStudent'], navigationExtras )
   }
+  initializeDB(){
+    this.service.initializeDB().subscribe((response) => {
+      console.log('DB is Initialized')
+    }, (error) => {
+      console.log('ERROR - ', error)
+    })
+  }
 
   getStudentData(){
     this.service.getStudentData().subscribe((response)=>{
